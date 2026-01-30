1c9b643f48e25306f3d0937fe37d0bf4ff59b93a
mohamad jalim <mohamadjalim05@gmail.com>
2026-01-30 12:59:47 +0100

    EAM-29 Create React project using Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center bg-linear-to-br from-slate-500 to-cyan-200 min-h-screen ">
+      <h1 className="font-bold " >welcom to Enterprise Asset Manager</h1>
+    </div>
+  )
+}
+ 
